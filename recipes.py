56dffc25ff0432a12d76da0a7a6ebb4e7718c015
veesusmikelheir <memebigboy27@hotmail.com>
2025-04-01 15:12:09 -0400

    started adding favorites functionality

--- recipes.py
+++ recipes.py
@@ -14,6 +14,41 @@ def save_recipe(mongo, recipe):
                                           }
                                          },upsert=True)
 
+def test_recipes_stuff(mongo):
+    user = mongo.db.users.find_one({'email': 'daniel@email.com'})
+    add_favorite(mongo, user['_id'],"test")
+    print(mongo.db.users.update_one({'email':'daniel@email.com'},
+                             {
+                                 '$pull':{"favorites":"dd"}
+                             }
+                             ))
+
+def search_recipes(mongo, name, category, flavor, difficulty):
+
+    pass
+    
+def add_favorite(mongo, userid, recipeid):
+    user = mongo.db.users.find_one({'_id': userid})
+    if not user:
+        return
+    
+    return mongo.db.users.update_one({'_id':userid},
+                             {
+                                 '$addToSet':{"favorites":recipeid}
+                             }
+                             )
+
+def remove_favorite(mongo, userid, recipeid):
+    user = mongo.db.users.find_one({'_id': userid})
+    if not user:
+        return
+    
+    return mongo.db.users.updateOne({'_id':userid},
+                             {
+                                 '$addToSet':{"favorites":recipeid}
+                             }
+                             )
+
 def get_recipes(mongo, recipeid):
     
     return mongo.db.recipes.find()
