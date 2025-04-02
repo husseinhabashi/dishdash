63d528db1fbec047fc2cfd1bfeeb109f5e4814c9
Veesus <memebigboy27@hotmail.com>
2025-04-01 23:04:31 -0400

    Added basic favorites and recipes

---
+++ models/recipe.py
@@ -0,0 +1,94 @@
+from bson.objectid import ObjectId
+def save_recipe(db, recipe):
+
+    return db.recipes.update_one({'_id': recipe.recipeID},
+                                        {"$set": 
+                                         {
+                                         
+                                          'owner': recipe.ownerID,
+                                          'name' : recipe.name,
+                                          'image' : recipe.image,
+                                          'category' : recipe.category,
+                                          'flavor' : recipe.flavor,
+                                          'difficulty' : recipe.difficulty,
+                                          'description' : recipe.description
+                                          }
+                                         },upsert=True)
+
+def test_recipes_stuff(db):
+    user = db.users.find_one({'email': 'daniel@email.com'})
+    add_favorite(db, user['_id'],"test")
+    print(db.users.update_one({'email':'daniel@email.com'},
+                             {
+                                 '$pull':{"favorites":"dd"}
+                             }
+                             ))
+
+def search_recipes(db, name, category, flavor, difficulty):
+
+    pass
+    
+def get_favorites(db, userid):
+    user = db.users.find_one({'_id': userid})
+    if not user:
+        return
+    
+    return [Recipe(x) for x in db.recipes.find({'_id':{'$in':user.get('favorites',[])}})]
+
+
+def add_favorite(db, userid, recipeid):
+    user = db.users.find_one({'_id': userid})
+    if not user:
+        return
+    
+    return db.users.update_one({'_id':userid},
+                             {
+                                 '$addToSet':{"favorites":recipeid}
+                             }
+                             )
+
+def remove_favorite(db, userid, recipeid):
+    user = db.users.find_one({'_id': userid})
+    if not user:
+        return
+    
+    return db.users.update_one({'_id':userid},
+                             {
+                                 '$addToSet':{"favorites":recipeid}
+                             }
+                             )
+
+def get_recipes(db):
+    
+    return [Recipe(x) for x in db.recipes.find()]
+
+def get_recipe(db, id):
+    recipe = db.recipes.find_one({'_id':ObjectId(id)})
+    if recipe:
+        return Recipe(recipe)
+    
+
+
+
+class Recipe:
+    recipeID = None
+    ownerID = None
+    name = None
+    description = None
+    image = None
+    category = None
+    flavor = None
+    difficulty = None
+    
+    def __init__(self):
+        self.recipeID = ObjectId()
+        pass
+    def __init__(self, recipe_cursor):
+        self.recipeID = recipe_cursor['_id']
+        self.ownerID = recipe_cursor['owner']
+        self.name = recipe_cursor['name']
+        self.image = recipe_cursor['image']
+        self.category = recipe_cursor['category']
+        self.flavor = recipe_cursor['flavor']
+        self.difficulty = recipe_cursor['difficulty']
+        self.description = recipe_cursor.get('description',"Default description!")
--- routes/main_routes.py
+++ routes/main_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, current_app
-from dishdash.utils.helpers import get_current_user_document
-
+from flask import Blueprint, render_template, request, redirect, url_for, current_app
+from utils.helpers import get_current_user_document
+import models.recipe as recipe
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
@@ -12,8 +12,20 @@ def index():
 
 @main_bp.route('/recipes')
 def recipes():
-    return render_template('recipes.html')
+    
+    return render_template('recipes.html',recipes = recipe.get_recipes(current_app.db))
+
+@main_bp.route('/recipes/<recipe_id>')
+def recipe_from_id(recipe_id):
+    gotten_recipe = recipe.get_recipe(current_app.db,recipe_id)
+    if not gotten_recipe:
+        return redirect(url_for('main.recipes'))
+    return render_template('recipe_details.html',recipe = gotten_recipe)
 
 @main_bp.route('/favorites')
 def favorites():
-    return render_template('favorites.html')
+    user = get_current_user_document(current_app.users_collection)
+    if not user:
+        return redirect(url_for('auth.login'))
+    return render_template('favorites.html',recipes = recipe.get_favorites(current_app.db,user['_id']))
+
